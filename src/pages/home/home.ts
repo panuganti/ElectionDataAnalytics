2058806bdc103647b2545c1434ab6def0fedbcc7
panuganti <rajkiran.panuganti@gmail.com>
2017-06-30 11:36:28 -0700

    working version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,6 +17,7 @@ export class HomePage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   geoJson: any;
+  results: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public data: DataProvider, public color: ColorProvider) {
@@ -24,6 +25,8 @@ export class HomePage {
 
   async ionViewDidLoad() {
     this.geoJson = await this.data.getGeoJson();
+    var results = await this.data.getResults('2008', 'ac');
+    this.results = this.GenerateStyleMaps(results);
     this.setDefaultMap();
   }
 
@@ -32,54 +35,68 @@ export class HomePage {
     let mapOptions = { center: latLng, zoom: 7 };
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
     this.map.data.addGeoJson(this.geoJson);
+    let styleMaps: any = Enumerable.from(this.results);
     this.map.setMapTypeId('terrain');
     this.map.data.setStyle((feature) => {
       let id = feature.getProperty('ac');
-      console.log(id);
+      if (styleMaps.any(t => t.Id == id)) {
+        return styleMaps.first(t => t.Id == id).Style;
+      }
       return {
         strokeWeight: 0.5,
         fillOpacity: 0.7,
         strokeOpacity: 0.5,
-        fillColor: this.color.getColor("orange", Math.floor(Math.random() * (100 - 25 + 1)) + 25, 0, 100, 9),
+        fillColor: this.color.getColor("black", Math.floor(Math.random() * (100 - 25 + 1)) + 25, 0, 100, 9),
         title: id
       };
     })
   }
 
-}
-
-export class StyleMaps {
-  constructor(public color: ColorProvider) { }
-
   public GenerateStyleMaps(acResults: Result[]): any[] {
     let en = Enumerable.from(acResults);
     let acStyleMaps: any[] = [];
+    let self = this;
     en.forEach(element => {
+      console.log(element.Id);
       let votes = Enumerable.from(en.where(t => t.Id == element.Id).first().Votes)
       let party = votes.first(t => t.Position == 1).Party;
-      let partyColor = this.colorMap[party];
-      let styleMap = this.defaultStyle;
+      let partyColor = self.getColor(party);
+      let styleMap: AcStyleMap = { Id: -1, Style: {}};
+      styleMap.Style = self.defaultStyle;
       styleMap.Id = element.Id;
       let totalVotes = votes.select(t => t.Votes).toArray().reduce((a, b) => a + b);
       let margin = votes.first(t => t.Position == 1).Votes - votes.first(t => t.Position == 2).Votes;
       let marginPercent = Math.ceil((margin) * 100 / totalVotes);
-      let color = this.color.getColor(partyColor, marginPercent, 0, 25);
+      let color = self.color.getColor(partyColor, marginPercent, 0, 25);
       styleMap.Style = {
-        strokeWeight: this.defaultStyle.strokeWeight,
-        fillOpacity: this.defaultStyle.fillOpacity,
-        strokeOpacity: this.defaultStyle.strokeOpacity,
+        strokeWeight: self.defaultStyle.strokeWeight,
+        fillOpacity: self.defaultStyle.fillOpacity,
+        strokeOpacity: self.defaultStyle.strokeOpacity,
         fillColor: color
       }
       acStyleMaps.push(styleMap);
+//      debugger;
     });
     return acStyleMaps;
   }
 
-  colorMap: any = { 
-    "bjp" : "orange",
-    "inc" : "lightblue",
-    "jds" : "darkgreen",
-    "others": "black"
+
+  getColor(party: string) {
+    if (this.colorMap[party.toLowerCase()] != undefined) {
+      return this.colorMap[party.toLowerCase()];
+    }
+    console.log(party);
+    return "black";
+  }
+
+  colorMap: any = {
+    "bjp": "orange",
+    "inc": "blue",
+    "jds": "green",
+    "others": "black",
+    "bharatiya janta party": "orange",
+    "indian national congress": "blue",
+    "janata dal (secular)": "green"
   }
 
   defaultStyle: any = {
@@ -88,35 +105,39 @@ export class StyleMaps {
     strokeOpacity: 0.3,
     strokeColor: "white"
   };
-
 }
 
-	export class Result {		
-		Id: number;
-		Name: string;
-		Votes: CandidateVote[];
-		
-		constructor() { }
-		
-		GetWinner(): string {
-			var en = Enumerable.from(this.Votes);
-			return en.first(t=>t.Position == 1).Name;
-		}
-		
-		GetWinningParty(): string {
-			var en = Enumerable.from(this.Votes);
-			return en.first(t=>t.Position == 1).Party;
-		}
-	}
-		
-	export class CandidateVote {
-		Votes: number;
-		Position: number;
-		Name: string;
-		Party: string;
-	}
-	
-	export class Distribution {
-		AcNo: number;
-		Percent: number;
-	}
+export interface AcStyleMap {
+  Id: number;
+  Style: any;
+}
+
+export class Result {
+  Id: number;
+  Name: string;
+  Votes: CandidateVote[];
+
+  constructor() { }
+
+  GetWinner(): string {
+    var en = Enumerable.from(this.Votes);
+    return en.first(t => t.Position == 1).Name;
+  }
+
+  GetWinningParty(): string {
+    var en = Enumerable.from(this.Votes);
+    return en.first(t => t.Position == 1).Party;
+  }
+}
+
+export class CandidateVote {
+  Votes: number;
+  Position: number;
+  Name: string;
+  Party: string;
+}
+
+export class Distribution {
+  AcNo: number;
+  Percent: number;
+}
--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -10,11 +10,30 @@ export class DataProvider {
   }
 
   async getGeoJson() {
-    return await this.http.get('assets/data/karnataka.assembly.geo.json').map(res => res.json()).toPromise();
+    return await this.http.get('assets/data/geojsons/karnataka.assembly.geo.json').map(res => res.json()).toPromise();
   }
 
   async getPreDelimGeoJson() {
-    return await this.http.get('assets/data/predelimitation.karnataka.geo.json').map(res => res.json()).toPromise();
+    return await this.http.get('assets/data/geojsons/predelimitation.karnataka.geo.json').map(res => res.json()).toPromise();
+  }
+
+  async getResults(year: string, type: string) {
+    switch(year) {
+      case '2014':  
+        return await this.http.get('assets/data/results/2014.pcac.json').map(res => res.json()).toPromise();
+      case '2013':
+        return await this.http.get('assets/data/results/2013.json').map(res => res.json()).toPromise();
+      case '2009':
+        return await this.http.get('assets/data/results/2009.pcac.json').map(res => res.json()).toPromise();
+      case '2008':
+        return await this.http.get('assets/data/results/2008.json').map(res => res.json()).toPromise();
+      case '2004':
+        return await this.http.get('assets/data/results/2004.json').map(res => res.json()).toPromise();
+      case '1999':
+        return await this.http.get('assets/data/results/1999.json').map(res => res.json()).toPromise();
+      default: 
+        throw new DOMException();
+    }
   }
 
 }
